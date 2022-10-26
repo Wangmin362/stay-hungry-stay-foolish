ab156d5c2173932112b091473ca549ea07a0e943
wangmin <wangmin@skyguard.com.cn>
2022-10-26 11:43:05 +0800

    feat(golang): 测试filepath功能

---
+++ golang/path/filepath/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+)
+
+func TestBase(t *testing.T) {
+	name := "D:\\Project\\github\\k8s\\controller-runtime\\examples\\scratch-env\\main.go"
+	fmt.Printf("filepath.Base is: %s\n", filepath.Base(name))
+	abs, _ := filepath.Abs(name)
+	fmt.Printf("filepath.Abs is: %s\n", abs)
+	fmt.Printf("filepath.Dir is: %s\n", filepath.Dir(name))
+}
