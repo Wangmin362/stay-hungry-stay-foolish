3a9949a7291ca59d967fc1f90aff3ea8c6ecb2ee
wangmin <wangmin@skyguard.com.cn>
2023-03-21 20:49:13 +0800

    feat(goang-sdk-net): net库的简单测试

--- golang/sdk/net/01_simple_http_test.go
+++ golang/sdk/net/01_simple_http_test.go
@@ -1,9 +1,21 @@
 package net
 
 import (
+	"net/http"
 	"testing"
 )
 
-func TestHttp(t *testing.T) {
+func Handler(resp http.ResponseWriter, req *http.Request) {
+	resp.Write([]byte("hello test"))
+}
+
+func TestBasicHttp(t *testing.T) {
+	http.HandleFunc("/demo", Handler)
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestHttpMux(t *testing.T) {
 
 }
