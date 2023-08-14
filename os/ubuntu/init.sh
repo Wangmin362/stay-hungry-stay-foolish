e3110a33b3c531268d361e06bedb16f6048c0d67
wangmin <wangmin@skyguard.com.cn>
2023-08-14 17:34:19 +0800

    feat(dockerfile): 构建ubuntu开发环境

--- os/ubuntu/init.sh
+++ os/ubuntu/init.sh
@@ -3,7 +3,7 @@
 # 执行命令遇到错误就退出
 set -e
 # 脚本中遇到不存在的变量就退出
-set -u
+# set -u
 # 执行指令的时候，同时把指令输出，方便观察结果
 set -x
 # 执行管道的时候，如果前面的命令出错，管道后面的命令会停止
@@ -11,126 +11,105 @@ set -o pipefail
 
 # TODO 对于可能的虚拟机，需要考虑修改网卡的UUID以及MAC地址（VMWare克隆时，会自动修改Mac地址）
 
-HOSTNAME=centos-pattern
+HOSTNAME=ubuntu-pattern
 
-IPADDR=192.168.11.10
-NETMASK=255.255.255.0
+HELMVERSION=v3.12.2
+IPADDR=192.168.11.10/24
 GATEWAY=192.168.11.2
-ETH=ifcfg-ens32
+ETH=ens32
 
-GITVERSION=2.41.0
 GOLANGVERSION=1.20.6
 
 # 配置主机名
 hostnamectl set-hostname ${HOSTNAME}
 
 # 配置网络
-sed -i "s/ONBOOT=no/ONBOOT=yes/g" /etc/sysconfig/network-scripts/${ETH}
-sed -i "s/BOOTPROTO=dhcp/BOOTPROTO=static/g" /etc/sysconfig/network-scripts/${ETH}
-sed -i "s/ONBOOT=no/ONBOOT=yes/g" /etc/sysconfig/network-scripts/${ETH}
-if ! cat /etc/sysconfig/network-scripts/${ETH} | grep IPADDR; then
-  echo "IPADDR=${IPADDR}" >> /etc/sysconfig/network-scripts/${ETH}
-else
-  sed -i "s/IPADDR=.*/IPADDR=${IPADDR}/g" /etc/sysconfig/network-scripts/${ETH}
-fi
-if ! cat /etc/sysconfig/network-scripts/${ETH} | grep NETMASK; then
-  echo "NETMASK=${NETMASK}" >> /etc/sysconfig/network-scripts/${ETH}
-else
-  sed -i "s/NETMASK=.*/NETMASK=${NETMASK}/g" /etc/sysconfig/network-scripts/${ETH}
-fi
-if ! cat /etc/sysconfig/network-scripts/${ETH} | grep GATEWAY; then
-  echo "GATEWAY=${GATEWAY}" >> /etc/sysconfig/network-scripts/${ETH}
-else
-  sed -i "s/GATEWAY=.*/GATEWAY=${GATEWAY}/g" /etc/sysconfig/network-scripts/${ETH}
-fi
-if ! cat /etc/sysconfig/network-scripts/${ETH} | grep DNS1; then
-  echo "DNS1=${GATEWAY}" >> /etc/sysconfig/network-scripts/${ETH}
-else
-  sed -i "s/DNS1=.*/DNS1=${GATEWAY}/g" /etc/sysconfig/network-scripts/${ETH}
-fi
-cat /etc/sysconfig/network-scripts/${ETH}
-systemctl restart network
+tee /etc/netplan/00-installer-config.yaml << EOF
+network:
+  ethernets:
+    ${ETH}:
+      addresses:
+      - ${IPADDR}
+      nameservers:
+        addresses:
+        - 114.114.114.114
+        - 8.8.8.8
+        search: []
+      routes:
+      - to: default
+        via: ${GATEWAY}
+  version: 2
+  renderer: networkd
+EOF
+
+netplan apply
+
+# 关闭apt命令下载软件时进入交互界面询问是否需要重启服务
+sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 
 # yum源加速 设置为清华源
-sed -e 's|^mirrorlist=|#mirrorlist=|g' \
-    -e 's|^#baseurl=http://mirror.centos.org|baseurl=https://mirrors.tuna.tsinghua.edu.cn|g' \
-    -i.bak /etc/yum.repos.d/CentOS-*.repo
-yum install -y wget
-# 安装EPEL源，阿里云镜像加速
-wget -O /etc/yum.repos.d/epel.repo https://mirrors.aliyun.com/repo/epel-7.repo
-# 更新YUM源 如果出现：[Errno 14] HTTP Error 404 - Not Found错误，可以参考：https://blog.51cto.com/waxyz/5336025
-yum clean all && yum makecache && yum update -y
-yum provides '*/applydeltarpm'
-yum install -y deltarpm
+tee /etc/apt/sources.list << 'EOF'
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+
+deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+EOF
+
+apt update -y && apt upgrade -y
 
 # 安装软件
-yum install -y net-tools net-tools telnet nmap sysstat lrszs dos2unix bind-utils bridge-utils \
-    bash-completion vim jq psmisc nfs-utils yum-utils device-mapper-persistent-data lvm2 network-scripts tar \
-    iproute passwd openssl curl zlib-devel curl-devel bzip2-devel openssl-devel ncurses-devel gcc package automake \
-    autoconf make gcc-c++ cpio expat-devel gettext-devel zlib tree stress htop atop sysbench openssh-server chrony \
-    ipvsadm ipset conntrack libseccomp
-# 更新内核
-uname -sr
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum -y install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
-sed -i "s@mirrorlist@#mirrorlist@g" /etc/yum.repos.d/elrepo.repo
-sed -i "s@elrepo.org/linux@mirrors.tuna.tsinghua.edu.cn/elrepo@g" /etc/yum.repos.d/elrepo.repo
-yum remove kernel-tools-libs.x86_64 kernel-tools.x86_64 -y
-# 安装最新版本内核
-yum --disablerepo=\* --enablerepo=elrepo-kernel install kernel-ml.x86_64 -y
-yum --disablerepo=\* --enablerepo=elrepo-kernel install kernel-ml-tools.x86_64 -y
-## 安装稳定版本内核
-#yum --disablerepo=\* --enablerepo=elrepo-kernel install kernel-lt.x86_64 -y
-#yum --disablerepo=\* --enablerepo=elrepo-kernel install kernel-lt-tools.x86_64 -y
-grep "^menuentry" /boot/grub2/grub.cfg | cut -d "'" -f2
-grub2-editenv list
-sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/g' /etc/default/grub
-sed -i 's/GRUB_DEFAULT=.*/GRUP_DEFAULT=0/g' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
+apt install -y net-tools telnet sysstat bridge-utils bash-completion vim jq tar openssl iputils-ping lsof lvm2 \
+    dnsutils curl gcc automake autoconf make tree stress htop atop sysbench chrony ipvsadm ipset conntrack ufw git
 
-# 禁用防火墙
-sed -i 's/AllowZoneDrifting=yes/AllowZoneDrifting=no/g' /etc/firewalld/firewalld.conf
-systemctl restart firewalld.service
-systemctl disable firewalld.service
-systemctl status firewalld.service
+# 命令自动补全
+sed -i 's@^#if ! shopt -oq posix; then@if ! shopt -oq posix; then@g' /etc/bash.bashrc
+sed -i 's@#  if \[ -f /usr/share/bash-completion/bash_completion \]; then@  if \[ -f /usr/share/bash-completion/bash_completion \]; then@g' /etc/bash.bashrc
+sed -i 's@#    . /usr/share/bash-completion/bash_completion@    . /usr/share/bash-completion/bash_completion@g' /etc/bash.bashrc
+sed -i 's@#  elif \[ -f /etc/bash_completion \]; then@  elif \[ -f /etc/bash_completion \]; then@g' /etc/bash.bashrc
+sed -i 's@#    . /etc/bash_completion@    . /etc/bash_completion@g' /etc/bash.bashrc
+sed -i 's@#  fi@  fi@g' /etc/bash.bashrc
+sed -i 's@#fi@fi@g' /etc/bash.bashrc
+source /etc/bash.bashrc
 
-# 禁用SWAP分区
-sed -ri 's/.*swap.*/#&/' /etc/fstab
-swapoff -a && sysctl -w vm.swappiness=0
-cat /etc/fstab
-
-# 禁用SeLinux
-setenforce 0
-sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config
-sestatus
+# 由于ubuntu本身使用的内核较新，并且使用apt命令升级软件时，会自动更新操作系统内核版本，所以无需下载
 
 # 安装SSH服务
-yum install -y openssh-server
-sed -i 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
-sed -i 's/#AddressFamily any/AddressFamily any/g' /etc/ssh/sshd_config
-sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/g' /etc/ssh/sshd_config
-sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
-systemctl enable sshd.service && systemctl start sshd.service
+apt install -y openssh-server
+sed -i 's/^#Port.*/Port 22/g' /etc/ssh/sshd_config
+sed -i 's/^#AddressFamily.*/AddressFamily any/g' /etc/ssh/sshd_config
+sed -i 's/^#ListenAddress.*/ListenAddress 0.0.0.0/g' /etc/ssh/sshd_config
+sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
 
 # 同步时间
-yum install -y chrony
+apt install -y chrony
 timedatectl set-timezone "Asia/Shanghai"
-sed -i 's/0.centos.pool.ntp.org/ntp1.aliyun.com/'  /etc/chrony.conf
-sed -i 's/1.centos.pool.ntp.org/ntp2.aliyun.com/'  /etc/chrony.conf
-sed -i 's/2.centos.pool.ntp.org/ntp3.aliyun.com/'  /etc/chrony.conf
-sed -i 's/3.centos.pool.ntp.org/ntp4.aliyun.com/'  /etc/chrony.conf
-if ! cat /etc/chrony.conf | grep ntp5.aliyun.com; then
-  sed -i "7 a server ntp5.aliyun.com iburst\nserver ntp6.aliyun.com iburst\nserver ntp7.aliyun.com iburst\nserver 0.cn.pool.ntp.org iburst\nserver 1.cn.pool.ntp.org iburst\nserver 2.cn.pool.ntp.org iburst\nserver 3.cn.pool.ntp.org iburst\nserver time1.cloud.tencent.com iburst\nserver time2.cloud.tencent.com iburst\nserver time3.cloud.tencent.com iburst\nserver time4.cloud.tencent.com iburst\n\n"  /etc/chrony.conf
+sed -i 's/ntp.ubuntu.com/ntp1.aliyun.com/'  /etc/chrony/chrony.conf
+sed -i 's/0.ubuntu.pool.ntp.org/ntp2.aliyun.com/'  /etc/chrony/chrony.conf
+sed -i 's/1.ubuntu.pool.ntp.org/ntp3.aliyun.com/'  /etc/chrony/chrony.conf
+sed -i 's/2.ubuntu.pool.ntp.org/ntp4.aliyun.com/'  /etc/chrony/chrony.conf
+if ! cat /etc/chrony/chrony.conf | grep ntp5.aliyun.com; then
+  sed -i "24 a server ntp5.aliyun.com iburst\nserver ntp6.aliyun.com iburst\nserver ntp7.aliyun.com iburst\nserver 0.cn.pool.ntp.org iburst\nserver 1.cn.pool.ntp.org iburst\nserver 2.cn.pool.ntp.org iburst\nserver 3.cn.pool.ntp.org iburst\nserver time1.cloud.tencent.com iburst\nserver time2.cloud.tencent.com iburst\nserver time3.cloud.tencent.com iburst\nserver time4.cloud.tencent.com iburst\n\n"  /etc/chrony/chrony.conf
 fi
-systemctl enable chronyd && systemctl start chronyd && systemctl restart chronyd && systemctl status chronyd
+systemctl restart chronyd
 chronyc sources -v
 
-# K8S 网络配置
-cat > /etc/NetworkManager/conf.d/calico.conf << EOF
-[keyfile]
-unmanaged-devices=interface-name:cali*;interface-name:tunl*
-EOF
-systemctl restart NetworkManager
+# 禁用防火墙
+ufw disable
+ufw status
 
 # 内核优化
 ulimit -SHn 65535
@@ -157,7 +136,8 @@ ipt_rpfilter
 ipt_REJECT
 ipip
 EOF
-lsmod | grep -e ip_vs -e nf_conntrack
+
+
 
 # 优化内核参数
 cat <<EOF > /etc/sysctl.d/k8s.conf
@@ -204,29 +184,15 @@ overlay
 br_netfilter
 EOF
 
-systemctl restart systemd-modules-load.service
 
 # 安装git
-set +u
-yum remove -y git
-wget -P /opt https://mirrors.edge.kernel.org/pub/software/scm/git/git-${GITVERSION}.tar.xz --no-check-certificate && cd /opt && \
-    tar -xvf git-${GITVERSION}.tar.xz && \
-    rm -f  git-${GITVERSION}.tar.xz && \
-    rm -f /usr/bin/git &&  \
-    cd git-${GITVERSION} && \
-    sh configure --prefix=/usr/local/git all && \
-    make && \
-    make install &&  \
-    cd .. && rm -rf  git-${GITVERSION} && \
-    echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/profile && source /etc/profile &&  \
-    git --version && \
+git --version && \
     git config --global user.name "wangmin" && \
     git config --global user.email "wangmin@skyguard.com.cn" && \
     git config --global url.ssh://git@gitcdteam.skyguardmis.com/.insteadOf https://gitcdteam.skyguardmis.com/
 
 # 安装go
-set +u
-wget -P /opt https://dl.google.com/go/go${GOLANGVERSION}.linux-amd64.tar.gz && cd /opt && \
+wget https://mirrors.aliyun.com/golang/go${GOLANGVERSION}.linux-amd64.tar.gz?spm=a2c6h.25603864.0.0.a6b07c45FOi9wZ -O /opt/go${GOLANGVERSION}.linux-amd64.tar.gz && cd /opt && rm -rf go${GOLANGVERSION} && \
     mkdir go${GOLANGVERSION} &&  \
     tar -zxf go${GOLANGVERSION}.linux-amd64.tar.gz -C go${GOLANGVERSION} && \
     rm -f go${GOLANGVERSION}.linux-amd64.tar.gz && \
@@ -255,6 +221,11 @@ wget -P /opt https://dl.google.com/go/go${GOLANGVERSION}.linux-amd64.tar.gz && c
     go install github.com/go-delve/delve/cmd/dlv@latest && \
     # 安装wire自动注入工具
     go install github.com/google/wire/cmd/wire@latest
+
+wget https://files.m.daocloud.io/get.helm.sh/helm-${HELMVERSION}-linux-amd64.tar.gz
+tar -zxvf helm-${HELMVERSION}-linux-amd64.tar.gz && \
+    mv linux-amd64/helm /usr/local/bin/helm && \
+    helm help && rm -f helm-${HELMVERSION}-linux-amd64.tar.gz rm linux-amd64 -rf
 
 
 reboot
