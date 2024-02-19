67829f7e6ad56b401a7e1571c160da238e9e26cc
wangmin <wangmin@skyguard.com.cn>
2024-02-19 14:20:31 +0800

    feat(golang-sdk): 增加对于testing包的理解

---
+++ golang/sdk/test/foo/inner_foo_test.go
@@ -0,0 +1,25 @@
+// 这种测试方式为包内测试，包内测试可以访问所有方法、变量
+package foo
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ golang/sdk/test/foo/outter_foo_test.go
@@ -0,0 +1,28 @@
+// 这种测试方式为包外测试，包外测试无法访问包内私有方法、私有变量
+package foo_test
+
+import (
+	"github.com/golang/demo/golang/sdk/test/foo"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := foo.Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
