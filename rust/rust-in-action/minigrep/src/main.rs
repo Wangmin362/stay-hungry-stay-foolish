865869d69f2bb467228df2d78ffc7cb75ba7f690
wangmin <wangmin@skyguard.com.cn>
2024-02-28 18:17:43 +0800

    feat(rust): 新增bubbletea库

--- rust/rust-in-action/minigrep/src/main.rs
+++ rust/rust-in-action/minigrep/src/main.rs
@@ -1,7 +1,17 @@
-
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
+    #[derive(PartialEq, Debug)]
+    struct Point {
+      x: f64,
+      y: f64,
+    }
+
+    // 将Point实例装箱（放到堆内存）
+    let box_point = Box::new(Point { x: 0.0, y: 0.0 });
+    // 通过解引用操作符取出Point实例
+    let unboxed_point: Point = *box_point;
+    assert_eq!(unboxed_point, Point { x: 0.0, y: 0.0 });
+
+    // 通过Deref技术，直接解引用，获取到内部的值
+    assert_eq!(&box_point.y, 0.0);
+    assert_eq!(&box_point.x, 0.0);
 }
