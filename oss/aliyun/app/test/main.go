8fd0384163d3696dded6f9953579ac89eedb8ce0
wangmin <wangmin@skyguard.com.cn>
2024-03-20 08:23:52 +0800

    feat: 测试win10后台进程文件

--- oss/aliyun/app/test/main.go
+++ oss/aliyun/app/test/main.go
@@ -2,30 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/golang/demo/oss/aliyun/app/sync"
 	"os"
 	"time"
 )
 
 func main() {
-	home, err := sync.GetEnvVar("USERPROFILE")
-	if err != nil {
-		fmt.Printf("%s\n", err)
-		os.Exit(1)
-	}
 
-	dir := fmt.Sprintf("%s\\Documents\\test", home)
-	if err = os.MkdirAll(dir, os.ModePerm); err != nil {
+	dir := "app-logs"
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
 		fmt.Printf("%s\n", err)
 		os.Exit(1)
 	}
 
 	for {
-		file := fmt.Sprintf("%s\\%s.txt", dir, time.Now().Format("2006.01.02_15:04:05"))
-		_, err = os.Create(file)
+		file := fmt.Sprintf(".\\%s\\app测试_%s.txt", dir, time.Now().Format("2006.01.02_15_04_05"))
+		_, err := os.Create(file)
 		if err != nil {
 			os.Exit(1)
 		}
-		time.Sleep(5 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
 }
