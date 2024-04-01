76df76f8f72ed78452ab8b2bb8488790ea5a6fca
wangmin <wangmin@skyguard.com.cn>
2024-04-01 12:13:52 +0800

    feat: 解决标题级别问题

--- tools/vnote/repaire_level/main.go
+++ tools/vnote/repaire_level/main.go
@@ -28,12 +28,16 @@ func RepairDir(dir string) error {
 			return nil // 目的直接跳过
 		}
 
-		return RepairMarkdown(path)
+		if err = RepairMarkdown(path); err != nil {
+			return err
+		}
+
+		return RepairHighLight(path)
 	})
 }
 
 const (
-	pattern string = `(#+) (\d[\d|\.]*) (\d[\d|\.]*\.)`
+	levelPattern string = `(#+) (\d[\d|\.]*) (\d[\d|\.]*\.)`
 )
 
 func RepairMarkdown(path string) error {
@@ -48,7 +52,7 @@ func RepairMarkdown(path string) error {
 	}
 
 	fileData := string(bytes.Clone(rawData))
-	re := regexp.MustCompile(pattern)
+	re := regexp.MustCompile(levelPattern)
 	match := re.FindAllSubmatch(rawData, -1)
 	for _, group := range match {
 		raw := string(group[0])
@@ -56,6 +60,47 @@ func RepairMarkdown(path string) error {
 		num := string(group[2])
 
 		target := fmt.Sprintf(`%s %s`, level, num)
+		fileData = strings.ReplaceAll(fileData, raw, target)
+	}
+
+	if bytes.Equal(rawData, []byte(fileData)) {
+		return nil
+	}
+
+	if err = os.WriteFile(path, []byte(fileData), os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const (
+	lineHighLightPattern string = `(#+) (\d[\d|\.]*) (.*?\n)`
+)
+
+func RepairHighLight(path string) error {
+	// 当前同步的文件必须是以.md结尾，也就是当前文件必须是一个markdown格式的文件才进行修改
+	if filepath.Ext(path) != ".md" {
+		return nil
+	}
+
+	rawData, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	fileData := string(bytes.Clone(rawData))
+	re := regexp.MustCompile(lineHighLightPattern)
+	match := re.FindAllSubmatch(rawData, -1)
+	for _, group := range match {
+		raw := string(group[0])
+		level := string(group[1])
+		num := string(group[2])
+		title := string(group[3])
+
+		title = strings.ReplaceAll(title, "`", "")
+
+		target := fmt.Sprintf(`%s %s %s`, level, num, title)
 		fileData = strings.ReplaceAll(fileData, raw, target)
 	}
 
