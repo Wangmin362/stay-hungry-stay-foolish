74e53fdb8c3ed29a55f002edfd07408272c6c866
wangmin <wangmin@skyguard.com.cn>
2024-04-02 14:02:00 +0800

    feat: 增加系统调用

--- golang/sdk/syscall/main.go
+++ golang/sdk/syscall/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"syscall"
 )
 
+// 使用syscall包创建文件并写入内容
 func main() {
-	fmt.Println("hello world")
+	// 定义要创建的文件路径和内容
+	filePath := "example.txt"
+	content := []byte("Hello, syscall!")
+
+	// 使用syscall包打开文件，如果不存在则创建，返回文件描述符
+	fd, err := syscall.Open(filePath, syscall.O_CREAT|syscall.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer syscall.Close(fd) // 确保文件描述符最后被关闭
+
+	// 使用syscall包写入内容
+	_, writeErr := syscall.Write(fd, content)
+	if writeErr != nil {
+		panic(writeErr)
+	}
+
+	fmt.Println("File written successfully")
 }
