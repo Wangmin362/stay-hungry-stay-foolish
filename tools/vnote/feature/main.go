9454707d3127eb86902c3856d0da5fba286afa8e
wangmin <wangmin@skyguard.com.cn>
2024-04-30 09:59:08 +0800

    feat: 目录读取

--- tools/vnote/feature/main.go
+++ tools/vnote/feature/main.go
@@ -5,12 +5,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
-	path := "QA.MD"
-	ReadLinesV3(path)
+	path := "D:/Notebook/Vnote/Blog/10.Kubernetes/06.kube-controller-manager/00.组件"
+	ReadDir(path)
+}
+
+func ReadDir(dir string) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return nil
+		}
+
+		if dir == path {
+			return nil
+		}
+
+		if !strings.Contains(path, "源码") {
+			return nil
+		}
+
+		if strings.Contains(path, "总体概览") {
+			return nil
+		}
+
+		index := strings.Index(info.Name(), "——")
+		name := info.Name()[index+6:]
+		name = strings.ReplaceAll(name, ".md", "")
+
+		fmt.Println("- [ ] ", name)
+
+		return nil
+	})
 }
 
 // ReadLinesV3 reads all lines of the file.
