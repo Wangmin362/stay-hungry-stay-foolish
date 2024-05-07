f524b8def45e355a4b061ccc6415c2532ef69b13
wangmin <wangmin@skyguard.com.cn>
2024-05-07 22:14:50 +0800

    feat: 更新操作系统初始化脚本

--- os/ubuntu/init.sh
+++ os/ubuntu/init.sh
@@ -11,7 +11,7 @@ set -o pipefail
 
 # TODO 对于可能的虚拟机，需要考虑修改网卡的UUID以及MAC地址（VMWare克隆时，会自动修改Mac地址）
 
-HOSTNAME=ubuntu-pattern
+HOSTNAME=desktop
 
 HELMVERSION=v3.12.2
 IPADDR=192.168.11.10/24
@@ -49,30 +49,61 @@ sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /e
 
 # yum源加速 设置为清华源
 tee /etc/apt/sources.list << 'EOF'
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb http://cn.archive.ubuntu.com/ubuntu jammy main restricted
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy main restricted
 
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
-# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb http://cn.archive.ubuntu.com/ubuntu jammy-updates main restricted
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-updates main restricted
 
-deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-# deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb http://cn.archive.ubuntu.com/ubuntu jammy universe
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy universe
+deb http://cn.archive.ubuntu.com/ubuntu jammy-updates universe
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-updates universe
 
-# 预发布软件源，不建议启用
-# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-# # deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb http://cn.archive.ubuntu.com/ubuntu jammy multiverse
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy multiverse
+deb http://cn.archive.ubuntu.com/ubuntu jammy-updates multiverse
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-updates multiverse
+
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb http://cn.archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-backports main restricted universe multiverse
+
+deb http://cn.archive.ubuntu.com/ubuntu jammy-security main restricted
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-security main restricted
+deb http://cn.archive.ubuntu.com/ubuntu jammy-security universe
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-security universe
+deb http://cn.archive.ubuntu.com/ubuntu jammy-security multiverse
+# deb-src http://cn.archive.ubuntu.com/ubuntu jammy-security multiverse
 EOF
 
 apt update -y && apt upgrade -y
 
 # 安装软件
-apt install -y net-tools telnet sysstat bridge-utils bash-completion vim jq tar openssl iputils-ping lsof lvm2 \
-    dnsutils curl gcc automake autoconf make tree stress htop atop sysbench chrony ipvsadm ipset conntrack ufw git
+apt update -y && apt upgrade -y && \
+    DEBIAN_FRONTEND=noninteractive TZ=Asia/Shanghai apt -y install tzdata && \
+    apt install -y man-db manpages-posix && apt install -y manpages-dev manpages-posix-dev && yes | unminimize && \
+    apt install -y net-tools telnet sysstat bridge-utils bash-completion vim jq tar openssl iputils-ping lsof lvm2 \
+    dnsutils curl gcc g++ automake autoconf make tree stress htop atop sysbench ipvsadm ipset conntrack ufw git \
+    build-essential flex libncurses-dev bison libelf-dev libssl-dev bc openssh-server ansible unzip binutils \
+    libc-ares-dev libtool pkg-config libsystemd-dev file texinfo nmap zsh autojump byobu language-pack-en \
+    language-pack-zh-hans python3-pygments chroma rsync libseccomp-dev
 
 # 命令自动补全
 sed -i 's@^#if ! shopt -oq posix; then@if ! shopt -oq posix; then@g' /etc/bash.bashrc
@@ -151,6 +182,10 @@ fs.inotify.max_user_watches=89100
 fs.file-max=52706963
 fs.nr_open=52706963
 net.netfilter.nf_conntrack_max=2310720
+
+fs.inotify.max_queued_events=1048576
+fs.inotify.max_user_watches=1048576
+fs.inotify.max_user_instances=1048576
 
 net.ipv4.tcp_keepalive_time = 600
 net.ipv4.tcp_keepalive_probes = 3
