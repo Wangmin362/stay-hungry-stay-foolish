635a5e05445a2d29f1f84267c26884dba1bb7a5a
wangmin <wangmin@skyguard.com.cn>
2024-05-09 12:54:03 +0800

    leetcode: 0704 二分查找

--- leetcode/09_binary_search/0704_二分查找_test.go
+++ leetcode/09_binary_search/0704_二分查找_test.go
@@ -1,27 +1,44 @@
 package _9_binary_search
 
-import "testing"
+import (
+	"testing"
+)
 
-// 分析：区间为[left, right]
-func search(nums []int, target int) int {
-	// 下面的两个检测条件可以不需要，因为根本就不满足for循环条件
-	//if nums == nil {
-	//	return -1
-	//}
-	//if len(nums) == 0 {
-	//	return -1
-	//}
+// 目标：给定一个有序数组，没有重复的元素，在给定一个target，找到给定target在数组中的下标位置，如果不存在， 直接返回-1；
+// 分析：由于是有序数组，且没有重复，所以可以直接使用二分查找，以log(n)的速度找到。
+// 解题思路：解法一：区间定义为[left, right]全比去结案，解法二：[left, right),区间定义为左闭右开的区间。
+// TODO 思考，如果存在重复元素，并且返回给定target的所有位置应该如何做？
 
+// 先实现第一种，全闭区间 [left, right]， 那么中位数大于target，则right=middle-1, 如果中位数小于target，则left=middle+1
+func searchAllClose(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
-	for left <= right { // 由于是闭区间[left, right]，因此left=right时，区间依然有效，即只有一个值
-		middle := left + (right-left)>>1
-		if nums[middle] == target {
-			return middle
-		} else if nums[middle] > target {
-			right = middle - 1 // 由于中位数比目标大，因此目标一定在中位数左边，所以区间为[left, middle-1]
-		} else if nums[middle] < target {
-			left = middle + 1 // 由于中位数比目标小，因此目标一定在中位数右边，所以区间为[middle+1, left]
+	for left <= right { // 由于是全闭区间，所以[left, right]是有效的，该区间只包含了一个数字
+		mid := (right - left) >> 1 // 等价于 (right + left) / 2，这里为了防止溢出
+		if nums[mid] > target {    // 中间的位置大于target，说明target在中间位置的左边，所以区间取中间位置左边
+			right = mid - 1
+		} else if nums[mid] < target { // 中间的位置小于target，说明target在中间位置右边，区间取中间位置的右边
+			left = mid + 1
+		} else { // 说明中间这个位置的数就是target
+			return mid
+		}
+	}
+	return -1 // 如果left > right，说明已经遍历完了，此时还没找到target，说明数组中不存在target，直接返回-1
+}
+
+// 这里实现第二种，即[left, right)左闭右开区间。mid为中间位置，若mid<target，说明target在右边，那么left=mid+1；若mid>target，说明target
+// 在左边，那么right = mid
+func searchRightOpen(nums []int, target int) int {
+	left := 0
+	right := len(nums)
+	for left < right { // 因为考虑的是[left, right)区间，因此left=right时，区间无效，因此不考虑相等
+		mid := (right - left) >> 1
+		if nums[mid] > target { // 中间的数大于target，所以取左边的区间，由于是左开右闭，因此时mid，而不是mid-1
+			right = mid
+		} else if nums[mid] < target { // 中间的数小于target，所以取右边的区间，由于是左开右闭，因此时mid+1
+			left = mid + 1
+		} else {
+			return mid
 		}
 	}
 	return -1
@@ -43,9 +60,9 @@ func TestBinarySearch(t *testing.T) {
 	}
 
 	for _, test := range twoSumTest {
-		get := search(test.array, test.target)
+		get := searchAllClose(test.array, test.target)
 		if test.expect != get {
-			t.Errorf("arr:%v, target:%v, expect:%v, get:%v", test.array, test.target, test.expect, get)
+			t.Fatalf("arr:%v, target:%v, expect:%v, get:%v", test.array, test.target, test.expect, get)
 		}
 	}
 }
