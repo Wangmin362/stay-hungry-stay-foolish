08bd1cff430de488fcd8953e9921d234f19d0523
wangmin <wangmin@skyguard.com.cn>
2024-05-21 10:07:21 +0800

    leetcode: 栈

--- leetcode/18_stack/0232_用栈实现队列_test.go
+++ leetcode/18_stack/0232_用栈实现队列_test.go
@@ -1,37 +1,55 @@
 package _1_array
 
+import (
+	"fmt"
+	"testing"
+)
+
 // 题目：https://leetcode.cn/problems/implement-queue-using-stacks/description/
 
 type MyQueue struct {
-	arr []int
+	in  []int
+	out []int
 }
 
 func Constructor() MyQueue {
-	return MyQueue{arr: make([]int, 0)}
+	return MyQueue{}
 }
 
 func (this *MyQueue) Push(x int) {
-	this.arr = append(this.arr, x)
+	this.in = append(this.in, x)
 }
 
 func (this *MyQueue) Pop() int {
-	if len(this.arr) > 0 {
-		return 0
+	if len(this.out) == 0 && len(this.in) != 0 {
+		for idx := len(this.in) - 1; idx >= 0; idx-- {
+			this.out = append(this.out, this.in[idx])
+		}
+		this.in = []int{}
 	}
-	num := this.arr[0]
-	this.arr = this.arr[1:]
 
-	return num
+	idx := len(this.out) - 1
+	x := this.out[idx]
+	this.out = this.out[:idx]
+	return x
 }
 
 func (this *MyQueue) Peek() int {
-	if len(this.arr) > 0 {
-		return 0
-	}
-
-	return this.arr[0]
+	x := this.Pop()
+	this.Push(x)
+	return x
 }
 
 func (this *MyQueue) Empty() bool {
-	return len(this.arr) == 0
+	return len(this.in) == 0 && len(this.out) == 0
+}
+
+func TestMyQueue(t *testing.T) {
+	qu := MyQueue{}
+
+	qu.Push(1)
+	qu.Push(2)
+	fmt.Println(qu.Peek())
+	fmt.Println(qu.Pop())
+	fmt.Println(qu.Empty())
 }
