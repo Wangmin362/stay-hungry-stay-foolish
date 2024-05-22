26cc1697c362bf60836e60ac9cd28e9c1b16f374
wangmin <wangmin@skyguard.com.cn>
2024-05-22 19:02:20 +0800

    golang: socks5代理

--- golang/network-program/socks5/client02/main.go
+++ golang/network-program/socks5/client02/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/http/httpproxy"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 func main() {
+	hpc := httpproxy.Config{
+		HTTPProxy:  "socks5://172.30.3.224:10805",
+		HTTPSProxy: "socks5://172.30.3.224:10805",
+	}
+
 	clt := http.Client{
 		Timeout: 5 * time.Second,
 		Transport: &http.Transport{
@@ -17,7 +24,9 @@ func main() {
 			// 配置环境变量 HTTPS_PROXY=socks5://192.168.11.224:10801  支持SOCKS5代理
 			// 配置环境变量 HTTPS_PROXY=192.168.11.224:10801           支持HTTPS代理
 			// 即可实现SOCKS代理
-			Proxy: http.ProxyFromEnvironment,
+			Proxy: func(r *http.Request) (*url.URL, error) {
+				return hpc.ProxyFunc()(r.URL)
+			},
 		},
 	}
 	resp, err := clt.Get("http://www.baidu.com")
