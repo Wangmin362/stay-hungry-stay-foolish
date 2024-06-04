fbe2a52701f9b02b9f82487e20f9dd70ef56733e
wangmin <wangmin@skyguard.com.cn>
2024-06-04 17:57:53 +0800

    golang: 测试断点结束的时候是否会发送SIGTERM信号

--- golang/feature/concurrency/channel/t-sigterm02/main.go
+++ golang/feature/concurrency/channel/t-sigterm02/main.go
@@ -10,11 +10,27 @@ import (
 
 func main() {
 	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	ctx22, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	ctx333, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		for {
 			<-ctx.Done()
 			fmt.Println("stopping")
+			return
+		}
+	}()
+	go func() {
+		for {
+			<-ctx22.Done()
+			fmt.Println("stopping222")
+			return
+		}
+	}()
+	go func() {
+		for {
+			<-ctx333.Done()
+			fmt.Println("stopping333")
 			return
 		}
 	}()
