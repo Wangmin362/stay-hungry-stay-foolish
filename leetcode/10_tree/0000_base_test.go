cf1c62712af5be57f21a3b56a06111d353c44323
wangmin <wangmin@skyguard.com.cn>
2024-07-02 11:30:12 +0800

    leetcode: 二叉树

---
+++ leetcode/10_tree/0000_base_test.go
@@ -0,0 +1,64 @@
+package _1_array
+
+import (
+	"container/list"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+// TreeNode 二叉树定义
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// Node N叉树定义
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func MakeTreeFromArray(arrStr string) *TreeNode {
+	var arr []string
+	for _, s := range strings.Split(arrStr, ",") {
+		arr = append(arr, s)
+	}
+
+	if len(arr) == 0 || arr[0] == "null" {
+		return nil
+	}
+
+	queue := list.New()
+	atoi, _ := strconv.Atoi(arr[0])
+	tree := &TreeNode{Val: atoi}
+	queue.PushBack(tree)
+	queue.PushBack(&TreeNode{Val: 0})
+	for queue.Len() > 0 {
+		node := queue.Remove(queue.Front()).(*TreeNode)
+		idx := queue.Remove(queue.Front()).(*TreeNode).Val
+
+		lidx := 2*idx + 1
+		if lidx < len(arr) && arr[lidx] != "null" {
+			atoi, _ = strconv.Atoi(arr[lidx])
+			node.Left = &TreeNode{Val: atoi}
+			queue.PushBack(node.Left)
+			queue.PushBack(&TreeNode{Val: lidx})
+		}
+		ridx := 2*idx + 2
+		if lidx+1 < len(arr) && arr[ridx] != "null" {
+			atoi, _ = strconv.Atoi(arr[ridx])
+			node.Right = &TreeNode{Val: atoi}
+			queue.PushBack(node.Right)
+			queue.PushBack(&TreeNode{Val: ridx})
+		}
+	}
+
+	return tree
+}
+
+func TestMakeTree(t *testing.T) {
+	arrStr := "5,4,8,11,null,13,4,7,2,null,null,5,1"
+	MakeTreeFromArray(arrStr)
+}
--- leetcode/10_tree/0113_路径总和II_test.go
+++ leetcode/10_tree/0113_路径总和II_test.go
@@ -1,20 +1,75 @@
 package _1_array
 
+import (
+	"reflect"
+	"testing"
+)
+
 // 地址：https://leetcode.cn/problems/path-sum-ii/
 
-// 前序遍历
+// 前序遍历 递归
 func pathSum01(root *TreeNode, targetSum int) [][]int {
-	// golang中的slice时值拷贝，只要扩容了，底层数组就会发生变化，因此这里不能使用切片作为参数
+	// golang中的slice是值拷贝，只要扩容了，底层数组就会发生变化，因此这里不能使用切片作为参数
 	//var tarversal func(node *TreeNode, []int, sum int)
+	if root == nil {
+		return nil
+	}
 
-	var traversal func(node *TreeNode, sum int)
-	traversal = func(node *TreeNode) {
-		if node.Left == nil && node.Right == nil {
+	var traversal func(node *TreeNode, sum int) // sum节点只包含当前节点之前节点的综合
+	var path []int
+	var res [][]int
+	traversal = func(node *TreeNode, sum int) {
+		sum += node.Val
+		path = append(path, node.Val)
+		if node.Left == nil && node.Right == nil && sum == targetSum {
+			tmp := make([]int, len(path))
+			for idx, v := range path {
+				tmp[idx] = v
+			}
+			res = append(res, tmp)
+		}
 
+		if node.Left != nil {
+			traversal(node.Left, sum)
+			path = path[:len(path)-1]
+		}
+		if node.Right != nil {
+			traversal(node.Right, sum)
+			path = path[:len(path)-1]
 		}
 	}
+
+	traversal(root, 0)
+	return res
 }
 
-func pathSum02(root *TreeNode, targetSum int) [][]int {
+func TestPathSum(t *testing.T) {
+	case1 := &TreeNode{Val: 4,
+		Left:  &TreeNode{Val: 9, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 3}},
+		Right: &TreeNode{Val: 7, Left: &TreeNode{Val: 5}, Right: &TreeNode{Val: 6}},
+	}
+	//case2 := &TreeNode{Val: 4,
+	//	Left:  &TreeNode{Val: 9, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 2}},
+	//	Right: &TreeNode{Val: 7, Right: &TreeNode{Val: 6}},
+	//}
+	//case3 := &TreeNode{Val: 1,
+	//	Right: &TreeNode{Val: 3, Left: &TreeNode{Val: 2}},
+	//}
 
+	var twoSumTest = []struct {
+		array  *TreeNode
+		target int
+		expect [][]int
+	}{
+		{array: case1, expect: [][]int{{4, 9, 3}, {4, 7, 5}}, target: 16},
+		//{array: case2, expect: []int{3, 9, 2, 4, 7, 6}},
+		//{array: case3, expect: []int{1, 2, 3}},
+	}
+
+	for _, test := range twoSumTest {
+		get := pathSum01(test.array, test.target)
+		if !reflect.DeepEqual(get, test.expect) {
+			t.Fatalf("expect:%v, get:%v", test.expect, get)
+		}
+	}
 }
