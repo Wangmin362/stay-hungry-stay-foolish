37174fc2583dcd2461b8aa904d8e7aadc5f8e52c
wangmin <wangmin@skyguard.com.cn>
2024-09-11 14:45:44 +0800

    leetcode: 动态规划

--- leetcode/04_dynamic_programming/0343_整数拆分_test.go
+++ leetcode/04_dynamic_programming/0343_整数拆分_test.go
@@ -1,33 +1,48 @@
 package _0_basic
 
 import (
-	"fmt"
 	"testing"
 )
 
 // https://leetcode.cn/problems/integer-break/description/
 
+// 题目分析：一个证书n，可以拆分为(1, n-1), (2, n-2), (3, n-3), (4, n-4), (5, n-5), ... (n-1, 1)
+// 那么 dp[n] = max(dp[n-1]*1, dp[n-2]*2, dp[n-3]*3, dp[n-4]*4, ...dp[2]*(n-2), dp[1]*(n-1))
+// 明确定义：dp[n]为数字n可以拆分为整数的最大值
+// 转移方程：dp[n] = max(dp[n-1]*1, dp[n-2]*2, dp[n-3]*3, dp[n-4]*4, ...dp[2]*(n-2), dp[1]*(n-1))
+// 初始化：dp[0]=0, dp[1]=1, dp[2]=1, dp[3]=2
+// 遍历顺序：从前往后
+// dp数组大小：[0, n] => n+1
+// 返回值：dp[n]
+
 func integerBreak(n int) int {
-	// 假设n = k1 + k2 + k3 + k4
-	// 那么n一定可以拆分为1*(n-1) 2*(n-2) 3*(n-3) 4*(n-4)
-	// 那么dp[i] = max(x*(i-x)) x=[1,n-1]
 	dp := make([]int, n+1)
-	dp[0] = 0
-	dp[1] = 1
-	dp[2] = 1
+	dp[1], dp[2] = 1, 1
 	for i := 3; i <= n; i++ {
-		iMax := 0
-		for x := 1; x < i; x++ {
-			iMax = max(iMax, x*dp[i-x]) // 拆分N个数字
-			iMax = max(iMax, x*(i-x))   // 拆分两个数字
+		for j := 1; j < i; j++ {
+			// dp[i-j]*j考虑的是把数字i拆分为多个数字的情况
+			// j*(i-j)考虑的是把数字拆分为两个数字的情况
+			dp[i] = max(dp[i], dp[i-j]*j, j*(i-j))
 		}
-		dp[i] = iMax
-		fmt.Println(dp)
 	}
 
 	return dp[n]
 }
 
 func TestIntegerBreak(t *testing.T) {
-	fmt.Println(integerBreak(4))
+	var testData = []struct {
+		n    int
+		want int
+	}{
+		{n: 2, want: 1},
+		{n: 3, want: 2},
+		{n: 10, want: 36},
+	}
+
+	for _, tt := range testData {
+		get := integerBreak(tt.n)
+		if get != tt.want {
+			t.Fatalf("n:%v, want:%v, get:%v", tt.n, tt.want, get)
+		}
+	}
 }
