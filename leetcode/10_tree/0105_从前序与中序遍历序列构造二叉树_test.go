4b63def61250fd29148d3a5df02a53d5c23621c3
wangmin <wangmin@skyguard.com.cn>
2024-09-20 13:47:48 +0800

    leetcode: top-interview-150

--- leetcode/10_tree/0105_从前序与中序遍历序列构造二叉树_test.go
+++ leetcode/10_tree/0105_从前序与中序遍历序列构造二叉树_test.go
@@ -7,42 +7,35 @@ import (
 
 // 地址：https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
 
-// 递归
 func buildTree(preorder []int, inorder []int) *TreeNode {
+	var build func(preorder []int, ps, pe int, inorder []int, is, ie int) *TreeNode
 
-	var traversal func(preorder []int, preBegin, preEnd int, inorder []int, inBegin, inEnd int) *TreeNode
-
-	// 前序遍历：中左右  3,9,20,15,7
-	// 中序遍历：左中右  9,3,15,20,7
-	traversal = func(preorder []int, preBegin, preEnd int, inorder []int, inBegin, inEnd int) *TreeNode {
-		if preBegin > preEnd || inBegin > inEnd {
+	build = func(preorder []int, ps, pe int, inorder []int, is, ie int) *TreeNode {
+		if ps > pe || is > ie {
 			return nil
 		}
-		node := &TreeNode{Val: preorder[preBegin]}
-		if preBegin == preEnd { // 如果只有一个节点，说明是叶子节点，直接返回
-			return node
-		}
-
-		// 切分中序遍历，找到左子树和右子树集合
-		idx := 0
-		for idx = inBegin; idx <= inEnd; idx++ {
-			if inorder[idx] == preorder[preBegin] {
-				break // 在中序遍历中已经找到了中序节点
+		root := &TreeNode{Val: preorder[ps]}
+		idx := is // 找到中序遍历，根节点的索引，这样可以区分左子树和右子树的集合
+		for idx <= ie {
+			if inorder[idx] == preorder[ps] {
+				break
 			}
+			idx++
 		}
-		length := idx - inBegin // 计算左子树一共有多少元素
-		preLimitor := preBegin + length
+		length := idx - is // 长度不应该计算中间节点
 
-		node.Left = traversal(preorder, preBegin+1, preLimitor, inorder, inBegin, idx-1) // taversal(前序遍历左子树，后序遍历左子树)
-		node.Right = traversal(preorder, preLimitor+1, preEnd, inorder, idx+1, inEnd)    // traversal(前序遍历右子树， 后序遍历右子树)
-		return node
-
+		root.Left = build(preorder, ps+1, ps+1+length-1, inorder, is, idx-1)
+		root.Right = build(preorder, ps+1+length-1+1, pe, inorder, idx+1, ie)
+		return root
 	}
 
-	return traversal(preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1)
+	return build(preorder, 0, len(preorder)-1, inorder, 0, len(inorder)-1)
 }
 
 func TestName(t *testing.T) {
 	tree := buildTree([]int{1, 2, 3}, []int{3, 2, 1})
+	fmt.Println(tree)
+
+	tree = buildTree([]int{3, 9, 20, 15, 7}, []int{9, 3, 15, 20, 7})
 	fmt.Println(tree)
 }
--- leetcode/10_tree/0106_从中序与后序遍历序列构造二叉树_test.go
+++ leetcode/10_tree/0106_从中序与后序遍历序列构造二叉树_test.go
@@ -7,39 +7,33 @@ import (
 
 // https://leetcode.cn/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
 
-func buildTree02(inorder []int, postorder []int) *TreeNode {
-	var traversal func(inorder []int, inBegin, inEnd int, postorder []int, postBegin, postEnd int) *TreeNode
+func buildTree106(inorder []int, postorder []int) *TreeNode {
+	var build func(inorder []int, is, ie int, postorder []int, ps, pe int) *TreeNode
 
-	traversal = func(inorder []int, inBegin, inEnd int, postorder []int, postBegin, postEnd int) *TreeNode {
-		if inBegin > inEnd || postBegin > postEnd {
+	build = func(inorder []int, is, ie int, postorder []int, ps, pe int) *TreeNode {
+		if is > ie || ps > ie {
 			return nil
 		}
 
-		// 中序 左中右 9,3,15,20,7
-		// 后续 左右中 9,15,7,20,3
-		node := &TreeNode{Val: postorder[postEnd]}
-		if inBegin == inEnd { // 只有一个节点
-			return node
-		}
-
-		// 切分中序遍历左右子树
-		idx := inBegin
-		for ; idx <= inEnd; idx++ {
-			if inorder[idx] == postorder[postEnd] {
+		root := &TreeNode{Val: postorder[pe]}
+		idx := is // 找到中序遍历根节点的索引号，拆分左子树和右子树的集合
+		for idx <= ie {
+			if inorder[idx] == postorder[pe] {
 				break
 			}
+			idx++
 		}
-		length := idx - inBegin // 左子树的长度
-		limitor := postBegin + length - 1
-		node.Left = traversal(inorder, inBegin, idx-1, postorder, postBegin, limitor)
-		node.Right = traversal(inorder, idx+1, inEnd, postorder, limitor+1, postEnd-1)
-		return node
+		length := idx - is // 左子树长度，不包含根节点，所以无需减一
+
+		root.Left = build(inorder, is, idx-1, postorder, ps, ps+length-1)
+		root.Right = build(inorder, idx+1, ie, postorder, ps+length, pe-1)
+		return root
 	}
 
-	return traversal(inorder, 0, len(inorder)-1, postorder, 0, len(postorder)-1)
+	return build(inorder, 0, len(inorder)-1, postorder, 0, len(postorder)-1)
 }
 
 func TestName11(t *testing.T) {
-	tree := buildTree02([]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3})
+	tree := buildTree106([]int{9, 3, 15, 20, 7}, []int{9, 15, 7, 20, 3})
 	fmt.Println(tree)
 }
