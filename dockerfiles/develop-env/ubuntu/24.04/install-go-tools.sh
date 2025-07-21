71f2f5ba8681eca6fcf628e5b3d6fc19d90ee0e2
wangmin <wangmin@riseunion.io>
2025-07-21 22:46:44 +0800

    feat: Dockerfile for dev

---
+++ dockerfiles/develop-env/ubuntu/24.04/install-go-tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+tools=(
+    # 开发工具
+    "sigs.k8s.io/kind@latest"
+    "golang.org/x/vuln/cmd/govulncheck@latest"
+    "google.golang.org/protobuf/cmd/protoc-gen-go@latest"
+    "google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest"
+    "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest"
+    "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest"
+
+    # 调试工具
+    "github.com/go-delve/delve/cmd/dlv@latest"
+    "github.com/google/wire/cmd/wire@latest"
+
+    # 代码分析
+    "honnef.co/go/tools/cmd/staticcheck@latest"
+    "golang.org/x/tools/gopls@latest"
+    "github.com/golangci/golangci-lint/cmd/golangci-lint@latest"
+)
+
+# 并行安装 (最多4个并发)
+printf "%s\n" "${tools[@]}" | xargs -P4 -I{} bash -c 'echo "Installing {}..." && go install -v {}'
